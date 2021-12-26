acc98e842a55426acf3fb04e9582138b0d07227b
mjcnn <96671963+mjcnn@users.noreply.github.com>
2021-12-26 14:55:10 -0500

    Add files via upload

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+
+module CNN {
+	opens cnn;
+	requires org.junit.jupiter.api;
+	requires junit;
+	requires aparapi;
+	requires org.junit.platform.commons;
+}
+
+                                                                                                                   
